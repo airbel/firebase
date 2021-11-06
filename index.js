2a7d4f808eec3e4278fbb1e8aea885275355f04f
Aiebel.ooo <geamdjum@gmail.com>
2021-11-06 10:09:11 +0800

    可以使用了

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const functions = require('./node_modules/firebase/firebase-firestore');
-const admin = require('./node_modules/firebase-admin');
-admin.initializeApp();
+var firebase = require("firebase-admin");
+var serviceAccount = require("./path/serverAccountKey.json");
 
-`/messages/:pushId/original`
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({original: original});
-    // Send back a message that we've successfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-  });
+firebase.initializeApp({
+  credential : firebase.credential.cert(serviceAccount),
+  databaseURL: "https://polor-ar-default-rtdb.asia-southeast1.firebasedatabase.app/"
+});
+
+var db = firebase.database();
+var ref = db.ref("data1/-MgLb41nTlzu_0hQrObV");
+ref.once("value",function(snapshot){
+
+  console.log (snapshot.val());
+})
 
 
-console.log ("end")
+console.log("hi")
