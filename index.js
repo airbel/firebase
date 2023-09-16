0c55b62c13d44d78130968602618fa57d7e7b067
Aiebel.ooo <geamdjum@gmail.com>
2023-09-16 18:29:47 +0800

    -v 005

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const da = new Date();
 console.log ("今天是",da.getFullYear(),"年", da.getMonth(),"月",da.getDay(),"日");
+const dateNow = da.getFullYear()+"年"+ da.getMonth()+"月"+da.getDay()+"日";
+var newpo ="";
+let getreflash = false;
+
+    var da_name = document.getElementById("name").value;
+    var da_address = document.getElementById("address").value;
+    var da_telphone = document.getElementById("telphone").value;
+    var da_postData = document.getElementById("dataposthere");
+
+function getHTMLdata (){
+     da_name = document.getElementById("name").value;
+     da_address = document.getElementById("address").value;
+     da_telphone = document.getElementById("telphone").value;
+     da_postData = document.getElementById("dataposthere");
+
+    console.log (da_name);
+    console.log (da_address);
+    console.log (da_telphone);
+    newpo = `<H3>${dateNow}</H3>
+            <H2>${da_name}</H2>
+            <H2>${da_address}</H2>
+            <H2>${da_telphone}</H2>`
+
+            
+            da_postData.innerHTML= newpo;
+
+}
+
+
+
