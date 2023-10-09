6ba7106816e097d0940009148f8a9991ec3fe001
Aiebel.ooo <geamdjum@gmail.com>
2023-10-09 11:50:46 +0800

    modlue_V1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import {x} from "/node_modules/friebase.js"
+
+
 const da = new Date();
-console.log ("今天是",da.getFullYear(),"年", da.getMonth(),"月",da.getDay(),"日");
-const dateNow = da.getFullYear()+"年"+ da.getMonth()+"月"+da.getDay()+"日";
+console.log ("今天是",da.getFullYear(),"年", da.getMonth(),"月",da.getDay()+1,"日");
+const dateNow = da.getFullYear()+"年"+ (da.getMonth()+1)+"月"+(da.getDay()+1)+"日";
 var newpo ="";
 let getreflash = false;
 
@@ -21,12 +24,13 @@ function getHTMLdata (){
     newpo = `<H3>${dateNow}</H3>
             <H2>${da_name}</H2>
             <H2>${da_address}</H2>
-            <H2>${da_telphone}</H2>`
-
-            
+            <H2>${da_telphone}</H2>`   
+    
+    
             da_postData.innerHTML= newpo;
 
 }
 
+var htq = ".JS 因為先執行，所以定義的資料找的到";
 
-
+console.log (x)
